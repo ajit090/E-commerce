76dff3ccdae1ec924cc295efc9fbeddc168e8d19
ajit <sajit4862@gmail.com>
2022-03-14 11:07:23 +0530

    Initail commit

--- e_commerce/website/views.py
+++ e_commerce/website/views.py
@@ -5,7 +5,6 @@ from .serializers import UserProfileSerializer,ItemSerializer,OrderItemSerialize
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 
 
